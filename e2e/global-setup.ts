fac379a0ba319a6cbed8406d711d4f238cdac2b0
summer-marie <summer.halsey0318@gmail.com>
2026-06-16 20:51:29 -0700

    test: add failing auth/family-access E2E tests (RED)

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../src/generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 import dotenv from 'dotenv'
 import fs from 'fs'
 import path from 'path'
@@ -13,7 +14,9 @@ const TEST_USER_NAME = 'E2E Test User'
 const AUTH_DIR = path.join(process.cwd(), 'e2e', '.auth')
 
 async function globalSetup() {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+  })
 
   try {
     const user = await prisma.user.upsert({
